a1f902c8bb6d2206bc4b4925d41531462bf2a4d6
Sirket Pc Furkan <f@f.com>
2025-10-27 19:29:44 +0300

    Refactor setup.py to condense setup function parameters into a single line

--- setup.py
+++ setup.py
@@ -4,7 +4,4 @@ Setup script for data4tr package
 
 from setuptools import setup, find_packages
 
-setup(
-    packages=find_packages(),
-    include_package_data=True,
-)
+setup(packages=find_packages(), include_package_data=True)
